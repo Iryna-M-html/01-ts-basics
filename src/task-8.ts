91cdf395ba55aacf87ca6c3509dac7ff8356ea9b
Iryna-M-html <ivmohnataya@gmail.com>
2025-08-24 21:22:07 +0300

    task 7-8

---
+++ src/task-8.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+}
+
+const post: Post = {
+  id: 1,
+  title:
+    "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+  body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+};
+
+async function fetchPosts() {
+  const response = await axios.get<Post[]>(
+    "https://jsonplaceholder.typicode.com/posts"
+  );
+  return response.data;
+}
+
+fetchPosts().then((posts) => {
+  console.log(posts[0].title);
+});
+console.log(post);
